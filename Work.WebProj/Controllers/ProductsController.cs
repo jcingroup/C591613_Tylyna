651bd38d7d571a2f910161f9d664658b3a819bf7
TinWu <yyuuix@gmail.com>
2016-10-12 18:59:00 +0800

    產品修改&購物車ok

--- Work.WebProj/Controllers/ProductsController.cs
+++ Work.WebProj/Controllers/ProductsController.cs
@@ -8,24 +8,24 @@ namespace DotWeb.Controllers
         // GET: Products
         public ActionResult Index()
         {
-            return View("Kind");
+            return View("Normal");
         }
-        // 咖啡品項(一般)
-        public ActionResult Kind()
+        // 咖啡品項(一般商品) - 分類九宮格，點選後進入咖啡介紹 Detail
+        public ActionResult Normal()
         {
             return View();
         }
-        // 季節限定
+        // 季節限定(限定商品) - 分類九宮格，點選後進入咖啡介紹 Detail
         public ActionResult Special()
         {
             return View();
         }
-        // 彈跳視窗(加入購物車)
+        // 咖啡介紹
         public ActionResult Detail()
         {
             return View();
         }
-        // 口味介紹-編輯器
+        // 品嚐LITA - all 商品列表
         public ActionResult Taste()
         {
             return View();
