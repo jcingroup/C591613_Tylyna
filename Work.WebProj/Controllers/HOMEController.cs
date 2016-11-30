f8f8877b4e33588ca2c6688198610daa27db3939
TinWu <yyuuix@gmail.com>
2016-11-30 15:09:08 +0800

    利他嚴選新增頁面

--- Work.WebProj/Controllers/HOMEController.cs
+++ Work.WebProj/Controllers/HOMEController.cs
@@ -11,8 +11,18 @@ namespace DotWeb.Controllers
         {
             return View();
         }
-        // 利他嚴選(嚴選製程、專業烘焙、安心認證)
+        // 利他嚴選(嚴選製程)
         public ActionResult Quality()
+        {
+            return View();
+        }
+        // 利他嚴選(專業烘焙)
+        public ActionResult Quality2()
+        {
+            return View();
+        }
+        // 利他嚴選(安心認證)
+        public ActionResult Quality3()
         {
             return View();
         }
