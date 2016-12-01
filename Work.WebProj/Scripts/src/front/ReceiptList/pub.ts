26a103ba82b4077ea614ad5de5fe970671de7a98
TinWu <yyuuix@gmail.com>
2016-12-01 15:12:33 +0800

    訂單狀態修改

--- Work.WebProj/Scripts/src/front/ReceiptList/pub.ts
+++ Work.WebProj/Scripts/src/front/ReceiptList/pub.ts
@@ -21,10 +21,10 @@ export interface IOrderState extends Search_Data {
 export const IOrderStateData: Array<IOrderState> = [
     { id: null, state: null, state_val: null, name: '全部' },
     { id: 1, state: 1, state_val: 0, name: '待付款' },
-    { id: 2, state: 1, state_val: 1, name: '已付款待確認' },
+    { id: 2, state: 1, state_val: 1, name: '付款待確認' },
     { id: 3, state: 1, state_val: 2, name: '已付款' },
     { id: 4, state: 2, state_val: 0, name: '待出貨' },
     { id: 5, state: 2, state_val: 1, name: '已出貨' },
-    { id: 6, state: 1, state_val: -1, name: '取消訂單' },
+    { id: 6, state: 1, state_val: -1, name: '訂單取消' },
     { id: 7, state: 3, state_val: null, name: '完成訂單' },
 ];
